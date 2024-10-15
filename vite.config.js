d9b6de45fa9e147ee935eb800b5c26069d808257
Ann <an.louc@gmail.com>
2024-10-15 10:54:44 +0300

    sync

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/11-3legane-e-commerce/",
+  base: "",
+  "homepage": "https://anka1010.github.io/11-3legane-e-commerce/"
 })
