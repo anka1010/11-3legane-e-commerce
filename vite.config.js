cc01bde3559ae627e31f6ab0c824909f4ad83da3
Ann Lukashenko <57502653+anka1010@users.noreply.github.com>
2024-10-16 10:56:56 +0300

    sync

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/11-3legane-e-commerce/",
   homepage: "https://anka1010.github.io/11-3legane-e-commerce/",
 });
